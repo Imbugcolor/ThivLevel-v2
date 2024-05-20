eb2eb51cecdd8d421a64e4427e87af284ad75205
Imbugcolor <viethd123456@gmail.com>
2024-05-20 16:32:44 +0700

    url config

--- client/src/components/mainpages/utils/config.js
+++ client/src/components/mainpages/utils/config.js
@@ -1,3 +1,4 @@
-// export const BASE_URL = 'https://thivlevelshop.onrender.com'
-export const BASE_URL = 'http://localhost:5000'
-export const CLIENT_URL = 'http://localhost:3000'
+export const BASE_URL = 'https://thivlevelshop.onrender.com'
+export const CLIENT_URL = 'https://thivlevelshop.onrender.com'
+// export const BASE_URL = 'http://localhost:5000'
+// export const CLIENT_URL = 'http://localhost:3000'
